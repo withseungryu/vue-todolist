baa6f85f629867e834767e7b75025fd960b939c9
withseungryu <alstmdsha@gmail.com>
2020-06-29 21:35:38 +0900

    Header 컴포넌트 생성, 동적 라우트 설정

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <ol class="breadcrumb">
+            <li><router-link to="/">모두</router-link></li>
+            <li><router-link to="/clear">끝난 활동</router-link></li>
+            <li class="active"><router-link to="/yet">진행 중인 활동</router-link></li>
+        </ol>
+    </div>
+</template> 
+
+<script lang="ts">
+    import {Vue, Component} from 'vue-property-decorator'
+
+    @Component
+    export default class Header extends Vue{
+    }
+    
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/_status.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="full_wrap">
+        <Header></Header>
+        <List></List>
+        {{hi}}
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+  import {Component} from 'vue-property-decorator'
+  import List from '~/components/List'
+  import Header from '~/components/Header'
+
+  @Component({
+    components: {
+        List,
+        Header
+    } 
+  })
+  export default class Index extends Vue{
+
+  }
+</script>
+
+<style scoped>
+    .full_wrap{
+        padding : 100px;
+    }
+</style>
